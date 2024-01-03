ea8d2769b38043afa3aceb87926abe328b8c5bfe
Julian S <200318@studierende.htl-donaustadt.at>
2024-01-03 17:05:41 +0100

    chore(settings): implement bottom glow setting functionality

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,4 +1,5 @@
 import { Cards } from '@/components/cards'
+import { useSettings } from '@/lib/hooks/use-settings'
 import Balancer from 'react-wrap-balancer'
 
 interface EmptyScreenProps {
@@ -6,11 +7,10 @@ interface EmptyScreenProps {
 }
 
 export function EmptyScreen({ children }: EmptyScreenProps) {
+  const { settings } = useSettings()
+
   return (
     <div className="w-full h-full overflow-auto md:overflow-hidden">
-      <div className="absolute bottom-0 w-full">
-        <div className="w-3/4 h-10 mx-auto bg-[radial-gradient(50%_50%_at_50%_50%,_rgba(185,_30,_35,_0.8)_46.35%,_rgba(173,_255,_0,_0)_100%)] mix-blend-lighten border-[35px] border-primary filter blur-[175px] rounded-full" />
-      </div>
       <div className="container flex flex-col justify-start h-full text-center md:justify-center">
         <div className="w-full mt-10 mb-5 text-5xl font-semibold md:my-5 text-secondary-foreground">
           <span className="hidden sm:inline bg-[linear-gradient(90deg,_rgba(9,_127,_77,_0.5)_0%,_rgba(185,_30,_35,_0.5)_0.01%,_rgba(185,_30,_35,_0)_100%)] border-l-primary border-l-4 pl-2">
@@ -34,6 +34,9 @@ export function EmptyScreen({ children }: EmptyScreenProps) {
       <div className="absolute bottom-0 inline w-full mt-5 md:hidden">
         {children}
       </div>
+      {settings.bottom_glow ? (
+        <div className="absolute bottom-0 inset-x-0 e w-3/4 z-100 h-10 mx-auto bg-[radial-gradient(50%_50%_at_50%_50%,_rgba(185,_30,_35,_0.8)_46.35%, _rgba(173,_255,_0,_0)_100%))] mix-blend-lighten border-[35px] border-red-600 filter blur-[175px] rounded-full" />
+      ) : null}
     </div>
   )
 }
