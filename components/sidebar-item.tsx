2253261cccfb5045f7dfb3dac99633aac5f03681
Julian S <200318@studierende.htl-donaustadt.at>
2024-01-26 02:05:36 +0100

    refactor(ui): remove unused animations
    remove unused framer motion animations

--- components/sidebar-item.tsx
+++ components/sidebar-item.tsx
@@ -3,12 +3,10 @@
 import * as React from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { motion } from 'framer-motion'
 import { buttonVariants } from '@/components/ui/button'
 import { IconMessage } from '@/components/ui/icons'
 import { type Chat } from '@/lib/types'
 import { cn } from '@/lib/utils'
-import { useLocalStorage } from '@/lib/hooks/use-local-storage'
 
 interface SidebarItemProps {
   index: number
@@ -20,83 +18,21 @@ export function SidebarItem({ index, chat, children }: SidebarItemProps) {
   const pathname = usePathname()
 
   const isActive = pathname === chat.path
-  const [newChatId, setNewChatId] = useLocalStorage('newChatId', null)
-  const shouldAnimate = index === 0 && isActive && newChatId
 
   if (!chat?.id) return null
 
   return (
-    <motion.div
-      className="relative w-full"
-      variants={{
-        initial: {
-          height: 0,
-          opacity: 0
-        },
-        animate: {
-          height: 'auto',
-          opacity: 1
-        }
-      }}
-      initial={shouldAnimate ? 'initial' : undefined}
-      animate={shouldAnimate ? 'animate' : undefined}
-      transition={{
-        duration: 0.25,
-        ease: 'easeIn'
-      }}
+    <Link
+      href={chat.path}
+      className={cn(
+        buttonVariants({ variant: 'ghost' }),
+        'relative group pr-10 transition-colors hover:bg-background-secondary h-12 my-1 max-w-full flex justify-start',
+        isActive && 'pr-12 font-medium bg-background-secondary'
+      )}
     >
-      <Link
-        href={chat.path}
-        className={cn(
-          buttonVariants({ variant: 'ghost' }),
-          'relative group w-full transition-colors hover:bg-background-secondary h-12 my-1',
-          isActive && 'pr-12 font-medium bg-background-secondary'
-        )}
-      >
-        <IconMessage className="mr-2 w-5 h-5 mb-[3px]" />
-        <div
-          className="relative flex-1 overflow-hidden select-none"
-          title={chat.title}
-        >
-          <span className="truncate">
-            {shouldAnimate ? (
-              chat.title.split('').map((character, index) => (
-                <motion.span
-                  key={index}
-                  variants={{
-                    initial: {
-                      opacity: 0,
-                      x: -100
-                    },
-                    animate: {
-                      opacity: 1,
-                      x: 0
-                    }
-                  }}
-                  initial={shouldAnimate ? 'initial' : undefined}
-                  animate={shouldAnimate ? 'animate' : undefined}
-                  transition={{
-                    duration: 0.25,
-                    ease: 'easeIn',
-                    delay: index * 0.05,
-                    staggerChildren: 0.05
-                  }}
-                  onAnimationComplete={() => {
-                    if (index === chat.title.length - 1) {
-                      setNewChatId(null)
-                    }
-                  }}
-                >
-                  {character}
-                </motion.span>
-              ))
-            ) : (
-              <span>{chat.title}</span>
-            )}
-          </span>
-        </div>
-        {isActive && <div className="absolute right-2">{children}</div>}
-      </Link>
-    </motion.div>
+      <IconMessage className="mr-2 w-5 h-5 mb-[3px]" />
+      <span className="flex-1 w-0 truncate ">{chat.title}</span>
+      {isActive && <div className="absolute right-2">{children}</div>}
+    </Link>
   )
 }
--- components/sidebar-items.tsx
+++ components/sidebar-items.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import { Chat } from '@/lib/types'
-import { AnimatePresence, motion } from 'framer-motion'
-
 import { SidebarActions } from '@/components/sidebar-actions'
 import { SidebarItem } from '@/components/sidebar-item'
 import { useChats } from '@/lib/hooks/use-chats'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 interface SidebarItemsProps {
   chats?: Chat[]
@@ -16,23 +15,15 @@ export function SidebarItems({ chats }: SidebarItemsProps) {
   if (!chats?.length) return null
 
   return (
-    <AnimatePresence>
+    <ScrollArea className="box-border overflow-auto">
       {chats.map(
         (chat, index) =>
           chat && (
-            <motion.div
-              key={chat?.id}
-              exit={{
-                opacity: 0,
-                height: 0
-              }}
-            >
-              <SidebarItem index={index} chat={chat}>
-                <SidebarActions chat={chat} removeChat={removeChat} />
-              </SidebarItem>
-            </motion.div>
+            <SidebarItem index={index} chat={chat} key={chat.id}>
+              <SidebarActions chat={chat} removeChat={removeChat} />
+            </SidebarItem>
           )
       )}
-    </AnimatePresence>
+    </ScrollArea>
   )
 }
