8d6c11d491cb4b8cf46c08f469ae9548c66c5cbb
Julian S <200318@studierende.htl-donaustadt.at>
2024-01-26 02:03:43 +0100

    fix(ui): fix mobile message layout

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -1,6 +1,3 @@
-// Inspired by Chatbot-UI and modified to fit the needs of this project
-// @see https://github.com/mckaywrigley/chatbot-ui/blob/main/components/Chat/ChatMessage.tsx
-
 import { Message } from 'ai'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
@@ -10,75 +7,124 @@ import { CodeBlock } from '@/components/ui/codeblock'
 import { MemoizedReactMarkdown } from '@/components/markdown'
 import { IconBRZ, IconUser } from '@/components/ui/icons'
 import { ChatMessageActions } from '@/components/chat-message-actions'
+import moment from 'moment'
+
+moment.locale('de')
 
 export interface ChatMessageProps {
   message: Message
 }
 
+// see https://stackoverflow.com/questions/35441820/tomorrow-today-and-yesterday-with-momentjs
+const dateFromNow = (date: Date) => {
+  const fromNow = moment(date).fromNow()
+
+  return moment(date).calendar(null, {
+    lastWeek: '[Letzte] dddd',
+    lastDay: '[Gestern um] HH:mm [Uhr]',
+    sameDay: '[Heute um] HH:mm [Uhr]',
+    nextDay: '[Morgen um] HH:mm [Uhr]',
+    nextWeek: 'dddd',
+    sameElse: function () {
+      return '[' + fromNow + ']'
+    }
+  })
+}
+
 export function ChatMessage({ message, ...props }: ChatMessageProps) {
   return (
     <div
-      className={cn('group relative mb-4 flex items-start xl:-ml-12 ')}
+      className={cn(
+        'group relative mb-4 flex-col md:flex-row items-start xl:-ml-12 '
+      )}
       {...props}
     >
-      <div
-        className={cn(
-          'flex h-10 w-10 shrink-0 select-none items-center justify-center rounded-md border shadow',
-          message.role === 'user'
-            ? 'bg-background'
-            : 'bg-background text-primary dark:bg-primary dark:text-background'
-        )}
-      >
-        {message.role === 'user' ? (
-          <IconUser />
-        ) : (
-          <IconBRZ className="w-7 h-7" />
-        )}
+      <div className="flex mb-5 ml-4 md:hidden">
+        <div
+          className={cn(
+            'h-10 w-10 shrink-0 select-none items-center justify-center rounded-md border shadow flex',
+            message.role === 'user'
+              ? 'bg-background'
+              : 'bg-background text-primary dark:bg-primary dark:text-background'
+          )}
+        >
+          {message.role === 'user' ? (
+            <div>
+              <IconUser />
+            </div>
+          ) : (
+            <IconBRZ className="w-7 h-7" />
+          )}
+        </div>
+        <div className="flex flex-col mx-3 text-sm">
+          {message.role === 'user' ? <b>Du</b> : <b>BRZ-Chatbot</b>}
+          <span className="text-muted-foreground">
+            {dateFromNow(message.createdAt ?? new Date())}
+          </span>
+        </div>
       </div>
-      {message.ui}
-      <div className="flex-1 px-1 ml-4 space-y-2 overflow-hidden">
-        <MemoizedReactMarkdown
-          className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
-          remarkPlugins={[remarkGfm, remarkMath]}
-          components={{
-            p({ children }) {
-              return <div className="w-full mb-2 last:mb-0">{children}</div>
-            },
-            code({ node, inline, className, children, ...props }) {
-              if (children.length) {
-                if (children[0] == '⬤') {
+      <div className="flex">
+        <div
+          className={cn(
+            'h-10 w-10 shrink-0 select-none items-center justify-center rounded-md border shadow md:flex hidden ',
+            message.role === 'user'
+              ? 'bg-background'
+              : 'bg-background text-primary dark:bg-primary dark:text-background'
+          )}
+        >
+          {message.role === 'user' ? (
+            <IconUser />
+          ) : (
+            <IconBRZ className="w-7 h-7" />
+          )}
+        </div>
+        {message.ui}
+        <div className="flex-1 px-1 ml-4 space-y-2 overflow-hidden">
+          <MemoizedReactMarkdown
+            className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
+            remarkPlugins={[remarkGfm, remarkMath]}
+            components={{
+              p({ children }) {
+                return <div className="w-full mb-2 last:mb-0">{children}</div>
+              },
+              code({ node, inline, className, children, ...props }) {
+                if (children.length) {
+                  if (children[0] == '⬤') {
+                    return (
+                      <span className="mt-1 cursor-default animate-pulse">
+                        ⬤
+                      </span>
+                    )
+                  }
+
+                  children[0] = (children[0] as string).replace(`'\`⬤\`'`, '⬤')
+                }
+
+                const match = /language-(\w+)/.exec(className || '')
+
+                if (inline) {
                   return (
-                    <span className="mt-1 cursor-default animate-pulse">⬤</span>
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
                   )
                 }
 
-                children[0] = (children[0] as string).replace(`'\`⬤\`'`, '⬤')
-              }
-
-              const match = /language-(\w+)/.exec(className || '')
-
-              if (inline) {
                 return (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
+                  <CodeBlock
+                    key={Math.random()}
+                    language={(match && match[1]) || ''}
+                    value={String(children).replace(/\n$/, '')}
+                    {...props}
+                  />
                 )
               }
-
-              return (
-                <CodeBlock
-                  key={Math.random()}
-                  language={(match && match[1]) || ''}
-                  value={String(children).replace(/\n$/, '')}
-                  {...props}
-                />
-              )
-            }
-          }}
-        >
-          {message.content}
-        </MemoizedReactMarkdown>
-        <ChatMessageActions message={message} className="hidden md:block" />
+            }}
+          >
+            {message.content}
+          </MemoizedReactMarkdown>
+          <ChatMessageActions message={message} className="hidden md:block" />
+        </div>
       </div>
     </div>
   )
